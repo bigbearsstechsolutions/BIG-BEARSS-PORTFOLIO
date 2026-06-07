3176ea84fa94ab7e5f326503189c2931b4f6e646
Big Bearss <bigbearsstechsolutions@gmail.com>
2026-06-07 16:44:46 +0530

    fix: remove handler line from server.js

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express")
 const cors = require("cors")
+const path = require("path")
 require("dotenv").config()
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+
+app.use(express.static(path.join(__dirname, "../FRONTEND/dist")))
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
@@ -12,14 +15,18 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Headers", "Content-Type")
   if (req.method === "OPTIONS") return res.sendStatus(200)
   next()
-})  // ✅ middleware yahan band karo
+})  
 
-// ✅ route bahar rakho
+
 app.post("/api/contact", require("./nodemailer"))
 
 // Test route — uncomment karo check ke liye
 app.get("/test", (req, res) => {
   res.json({ ok: true, message: "Server kaam kar raha hai!" })
+})
+
+app.get('/{*splat}', (req, res)=>{
+  res.sendFile(path.join(__dirname, "../FRONTEND/dist/index.html"))
 })
 
 const port = process.env.PORT || 5000
